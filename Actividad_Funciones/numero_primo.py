d2aebc53b758188c14824bf6a45e5ee129f36adf
Yareli7 <santoyoyareli77@gmail.com>
2026-07-28 23:36:41 -0600

    Agrega verificador de numero primo

---
+++ Actividad_Funciones/numero_primo.py
@@ -0,0 +1,20 @@
+# funcion para saber si un numero es primo
+def es_primo(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    # revisamos solo impares hasta la raiz
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+# programa principal
+num = int(input("Ingrese un número: "))
+if es_primo(num):
+    print("Es primo")
+else:
+    print("No es primo")
