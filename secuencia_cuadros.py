b0c32f94b82c19d0b69a090372c14c1614223f24
Yareli7 <santoyoyareli77@gmail.com>
2026-07-29 23:37:17 -0600

    Delete secuencia_cuadros.py

--- secuencia_cuadros.py
+++
@@ -1,9 +0,0 @@
-#Secuencia de cuadrados
-N = int(input("Número positivo: "))
-i = 1
-while True:
-    print(i ** 2) # calcula cuadrado
-    i += 1
-    if i > N:
-        break
-print("Secuencia de cuadrados hasta", N)
