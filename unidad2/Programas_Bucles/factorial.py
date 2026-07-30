efe11669c3eadcc1071684631e8bac679298e3ac
Yareli7 <santoyoyareli77@gmail.com>
2026-07-30 00:07:54 -0600

    Delete unidad2/Programas_Bucles directory

--- unidad2/Programas_Bucles/factorial.py
+++
@@ -1,9 +0,0 @@
-#Factorial
-num = int(input("Número para factorial: ")) 
-factorial = 1 # empieza en 1
-if num < 0: 
-    print("Factorial no definido para negativos")
-else: # si es positivo o 0
-    for i in range(1, num + 1): 
-        factorial *= i # multiplica
-    print("El factorial de", num, "es:", factorial) # resultado
